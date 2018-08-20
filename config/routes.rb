fe77ba9cde2ad6164141a317c8e284aa92612735
ishida_masakazu <u225210b@gmail.com>
2018-08-20 14:12:18 +0900

    usersテーブル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  
+
+  root 'static_pages#index'
+
   devise_for :users
+  resources :users
+
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end

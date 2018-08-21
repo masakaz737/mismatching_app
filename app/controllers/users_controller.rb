5df20b8c883cc139497164aada3759995bf33fe2
ishida_masakazu <u225210b@gmail.com>
2018-08-21 21:48:53 +0900

    プロフィール画像アップロード機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,29 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def show
-    @user = User.find(params[:id])
     @questionnaire = Questionnaire.find_by(user_id: @user.id)
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'icon was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :show }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:icon, :icon_cache)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  mount_uploader :icon, ImageUploader
+
   has_one :questionnaire, dependent: :destroy
 
   validates :name, presence: true

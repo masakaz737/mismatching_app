b40ac55946bfc9517dd603506610f8674d49e521
ishida_masakazu <u225210b@gmail.com>
2018-11-24 14:31:12 +0900

    relationships_controllerのメソッドをmodelへ移動

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,14 @@ class User < ApplicationRecord
   has_many :notifications, dependent: :destroy
   has_many :messages, dependent: :destroy
 
+
+  # (回答5 - 回答1) * 要素毎の設問数 * WEIGHT
+  MAX_GAP = (5 - 1) * 4 * 10
+  # (女2 - 男1) * 40 + (65歳 - 18歳) + (海外50 - 北海道1) + (学歴5 - 学歴1) * 10 + (職業5 - 職業1) * 10
+  MAX_GAP_BACKGROUND = (2 - 1) * 40 + (65 - 18) + (50 - 1) + (5 - 1) * 10 * 2
+
+  PERCENTAGE = 100 #100分率への変換
+
   #指定のユーザをフォローする
   def follow!(other_user)
     active_relationships.new(followed_id: other_user.id)
@@ -27,5 +35,34 @@ class User < ApplicationRecord
   #フォローしているかどうかを確認する
   def following?(other_user)
     active_relationships.find_by(followed_id: other_user.id)
+  end
+
+  def self.find_matching_user(user)
+    @questionnaires = Questionnaire.all.order(:id)
+    #各設問における他ユーザとの回答差を点数化した配列を取得
+    @positive_points, @faithful_points, @cooperative_points, @mental_points,
+    @curious_points = Questionnaire.questionnaire_calculation(user)
+    @background_points = Questionnaire.background_calculation(user)
+
+    total_points = [
+      @positive_points, @faithful_points, @cooperative_points, @mental_points,
+      @curious_points, @background_points
+    ].transpose.map { |n| n.inject(:+) }
+
+    #total_scoreが一番高いユーザーのインデックス番号を取得
+    maximum = total_points.index(total_points.max)
+
+    @matching_user = User.find(@questionnaires[maximum].user_id)
+    #マッチングしたユーザとのマッチング度合いを項目毎に100分率にて計算
+    @total_score = total_points.max * PERCENTAGE / (MAX_GAP * 5 + MAX_GAP_BACKGROUND)
+    @positive_score = @positive_points[maximum] * PERCENTAGE / MAX_GAP
+    @faithful_score = @faithful_points[maximum] * PERCENTAGE / MAX_GAP
+    @cooperative_score = @cooperative_points[maximum] * PERCENTAGE / MAX_GAP
+    @mental_score = @mental_points[maximum] * PERCENTAGE / MAX_GAP
+    @curious_score = @curious_points[maximum] * PERCENTAGE / MAX_GAP
+    @background_score = @background_points[maximum] * PERCENTAGE / MAX_GAP_BACKGROUND
+    binding.pry
+    return @matching_user, @total_score, @positive_score, @faithful_score,
+          @cooperative_score, @mental_score, @curious_score, @background_score
   end
 end
